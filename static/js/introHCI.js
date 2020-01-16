56f87e7818997b3b7cd53a88ccafba5383cd7e31
vhon0603 <41207326+vhon0603@users.noreply.github.com>
2020-01-16 13:37:22 -0800

    changes

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
     initializePage();
 });
 
+function projectClick(e){
+  e.preventDefault();
+  $(this).css("background-color", "#7fff00");
+}
+
 function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
